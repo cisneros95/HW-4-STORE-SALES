e028100a145bc72b983f39d634aa1e7475d0d612
Raul Cisneros <raul.f.cisneros@outlook.com>
2020-06-03 18:54:13 -0700

    Add more Comments!!!!

--- Program.cs
+++ Program.cs
@@ -18,27 +18,26 @@ namespace HW_4_WK_4
 
 
             string Stored = " ";
-
-
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 5; i++) // Allows the user to enter 5 values that will equal sales value
             {
-                string input1;
-                int input2, asteriskCount;
-                Console.WriteLine("Input number of sales please!: ");
-                input1 = Console.ReadLine();
-                Stored += input1 + "   is ";
+                
+                
+                Console.WriteLine("Input sales values for each of five stores please!: ");
+                string num1;                            // num1 input is a string
+                num1 = Console.ReadLine();              // num1 input is stored
+                Stored += num1 + "   is ";              //  Added a space after user's input and an "is" before astrisks output
 
-
-                input2 = Convert.ToInt32(input1);
-                asteriskCount = input2 / 1;
+                int num2, asteriskCount;
+                num2 = Convert.ToInt32(num1);           // Convert string to integer  
+                asteriskCount = num2 / 100;             // Divide integer by 100
                 for (int j = 0; j < asteriskCount; j++)
                 {
-                    Stored += "*";
+                    Stored += "*";                      // 
 
                 }
 
                 if (i != 4)
-                    Stored += "\n ";
+                    Stored += "\n ";                    // Adds the store inputs into 5 rows
 
             }
             Console.WriteLine(Stored);
